3261376aaa80929445e69cb95fe280c3a4e4b2e1
let1ci4b <leth4852@gmail.com>
2024-05-13 16:28:32 -0300

    ajustes de estrutura

--- DesafioControleFluxo/src/Contador.java
+++ DesafioControleFluxo/src/Contador.java
@@ -2,6 +2,7 @@ import java.util.*;
 public class Contador {
     public static void main(String[] args) {
         Scanner terminal = new Scanner(System.in);
+
         System.out.println("Digite o primeiro parâmetro");
         int parametroUm = terminal.nextInt();
         System.out.println("Digite o segundo parâmetro");
@@ -9,7 +10,7 @@ public class Contador {
 
         try {
             contar(parametroUm, parametroDois);
-        }catch (ParametrosInvalidosException e) {
+        } catch (ParametrosInvalidosException e) {
             System.out.println("O segundo parâmetro deve ser maior que o primeiro");
         }
 
